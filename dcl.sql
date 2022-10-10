73f217d83d0d57350c0d464b1d695b6419c6c832
MUSLIH SAHMAT <106954335+muslih2018@users.noreply.github.com>
2022-10-10 18:41:41 +0700

    Update dcl.sql

--- dcl.sql
+++ dcl.sql
@@ -1,5 +1,32 @@
-create user 'dosen'@'localhost' identified by 'dosen_';
-create user 'mahasiswa'@'localhost' identified by 'mahasiswa_';
 
-grant all privileges on *.*  to 'dosen'@'localhost';
-grant all privileges on *.*  to 'mahasiswa'@'localhost';
+#Dcl
+
+# Buat user baru dan set passwordnya
+CREATE USER 'DosenTeknik'@'localhost' IDENTIFIED BY 'teknikBersatu';
+
+# Beri hak akses ke user 
+GRANT CREATE, DROP, SELECT, INSERT, DELETE, UPDATE 
+ON kelompok5
+TO 'DosenTeknik'@'localhost';
+
+#beri semua hak  akses ke user
+grant all privileges on *.*  to 'DosenTeknik'@'localhost';
+
+
+# Beri akses tertentu (misal akses SELECT) kepada semua user
+GRANT SELECT 
+ON kelompok5
+TO '*'@'localhost';
+
+# Cek status hak akses user
+SHOW GRANTS FOR 'DosenTeknik'@'localhost';
+
+# Hapus hak akses user
+REVOKE CREATE, DROP 
+ON kelompok5
+TO 'DosenTeknik'@'localhost';
+
+# Hapus akses tertentu (misal akses DROP) kepada semua user
+REVOKE DROP  
+ON kelompok5
+TO '*'@'localhost';
