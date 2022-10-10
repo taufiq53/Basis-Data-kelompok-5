a523022a229eeac2630e77e94d4f4f76719fd908
Stefany Frans Sarira <114038304+stefanysarira@users.noreply.github.com>
2022-10-10 20:06:46 +0800

    Update dcl.sql

--- dcl.sql
+++ dcl.sql
@@ -1,32 +1,30 @@
 
 #Dcl
 
-# Buat user baru dan set passwordnya
+# Membuat user baru yaitu user dosen dan user mahasiswa, lalu set passwordnya
 CREATE USER 'DosenTeknik'@'localhost' IDENTIFIED BY 'teknikBersatu';
-
-# Beri hak akses ke user 
-GRANT CREATE, DROP, SELECT, INSERT, DELETE, UPDATE 
-ON kelompok5
-TO 'DosenTeknik'@'localhost';
-
-#beri semua hak  akses ke user
-grant all privileges on *.*  to 'DosenTeknik'@'localhost';
+CREATE USER 'Mahasiswa'@'localhost' IDENTIFIED BY 'Mahasiswa_';
 
 
-# Beri akses tertentu (misal akses SELECT) kepada semua user
+# Memberi semua hak akses ke user
+GRANT ALL PRIVILEGES ON *.*  TO 'DosenTeknik'@'localhost';
+GRANT ALL PRIVILEGES ON *.*  TO 'Mahasiswa'@'localhost';
+
+
+# Memberi akses tertentu (sebagai contoh akses SELECT) kepada semua user
 GRANT SELECT 
-ON kelompok5
+ON *.*
 TO '*'@'localhost';
 
-# Cek status hak akses user
+# Menampilkan status hak akses user
 SHOW GRANTS FOR 'DosenTeknik'@'localhost';
 
-# Hapus hak akses user
+# Menghapus hak akses user
 REVOKE CREATE, DROP 
-ON kelompok5
+ON *.*
 TO 'DosenTeknik'@'localhost';
 
-# Hapus akses tertentu (misal akses DROP) kepada semua user
+# Menghapus akses tertentu (sebagai contoh akses DROP) kepada semua user
 REVOKE DROP  
-ON kelompok5
+ON *.*
 TO '*'@'localhost';
